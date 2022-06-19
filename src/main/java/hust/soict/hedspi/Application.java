4343c3f4de401b42f5890ed32f26e8bbe5af31e2
hieugiabao <hieugiabao12@gmail.com>
2022-06-19 11:14:17 +0700

    init project

---
+++ src/main/java/hust/soict/hedspi/Application.java
@@ -0,0 +1,11 @@
+package hust.soict.hedspi;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module oolt.project {
+  
+}
