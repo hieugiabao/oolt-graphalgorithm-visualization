995ebd7f80c71d8c12f7bb9975aad27b7bdb4314
hieugiabao <hieugiabao12@gmail.com>
2022-06-19 14:16:47 +0700

    fix example graph

--- src/main/java/hust/soict/hedspi/model/graph/BaseGraph.java
+++ src/main/java/hust/soict/hedspi/model/graph/BaseGraph.java
@@ -110,7 +110,7 @@ public abstract class BaseGraph<E extends Edge> {
 
   public abstract Set<E> outgoingEdgesOf(Vertex v);
 
-  public static BaseGraph CP410() {
+  public static BaseGraph<Edge> CP410() {
     UndirectedGraph graph = new UndirectedGraph();
     Vertex v0 = new Vertex(0);
     Vertex v1 = new Vertex(1);
@@ -118,16 +118,19 @@ public abstract class BaseGraph<E extends Edge> {
     Vertex v3 = new Vertex(3);
     Vertex v4 = new Vertex(4);
 
-    graph.addEdge(v0, v1, 24);
-    graph.addEdge(v0, v2, 13);
-    graph.addEdge(v0, v3, 13);
-    graph.addEdge(v0, v4, 22);
-    graph.addEdge(v1, v2, 22);
-    graph.addEdge(v1, v3, 13);
-    graph.addEdge(v1, v4, 13);
-    graph.addEdge(v2, v3, 19);
-    graph.addEdge(v2, v4, 14);
-    graph.addEdge(v3, v4, 19);
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+
+    graph.addEdge(v0, v1, 4);
+    graph.addEdge(v0, v2, 4);
+    graph.addEdge(v0, v3, 6);
+    graph.addEdge(v0, v4, 6);
+    graph.addEdge(v1, v2, 2);
+    graph.addEdge(v2, v3, 8);
+    graph.addEdge(v3, v4, 9);
 
     return graph;
   }
