3d4930eeaf892018263a2390c85baf88aaa44bd4
nghiemtuan1411 <nghiemtuan1411@gmail.com>
2022-06-22 22:03:25 +0700

    addbaseGraph

--- src/main/java/hust/soict/hedspi/model/graph/BaseGraph.java
+++ src/main/java/hust/soict/hedspi/model/graph/BaseGraph.java
@@ -110,7 +110,7 @@ public abstract class BaseGraph<E extends Edge> {
 
   public abstract Set<E> outgoingEdgesOf(Vertex v);
 
-  public static BaseGraph<Edge> CP410() {
+  public static BaseGraph<? extends Edge> CP410() {
     UndirectedGraph graph = new UndirectedGraph();
     Vertex v0 = new Vertex(0);
     Vertex v1 = new Vertex(1);
@@ -131,6 +131,164 @@ public abstract class BaseGraph<E extends Edge> {
     graph.addEdge(v1, v2, 2);
     graph.addEdge(v2, v3, 8);
     graph.addEdge(v3, v4, 9);
+
+    return graph;
+  }
+  public static BaseGraph<? extends Edge> CP414() {
+    UndirectedGraph graph = new UndirectedGraph();
+    Vertex v0 = new Vertex(0);
+    Vertex v1 = new Vertex(1);
+    Vertex v2 = new Vertex(2);
+    Vertex v3 = new Vertex(3);
+    Vertex v4 = new Vertex(4);
+
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+
+    graph.addEdge(v0, v1, 9);
+    graph.addEdge(v0, v2, 75);
+    graph.addEdge(v1, v2, 95);
+    graph.addEdge(v1, v3, 19);
+    graph.addEdge(v1, v4, 42);
+    graph.addEdge(v2, v3, 51);
+    graph.addEdge(v3, v4, 31);
+
+    return graph;
+  }
+  public static BaseGraph<? extends Edge> K5() {
+    UndirectedGraph graph = new UndirectedGraph();
+    Vertex v0 = new Vertex(0);
+    Vertex v1 = new Vertex(1);
+    Vertex v2 = new Vertex(2);
+    Vertex v3 = new Vertex(3);
+    Vertex v4 = new Vertex(4);
+
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+
+    graph.addEdge(v0, v1, 24);
+    graph.addEdge(v0, v2, 13);
+    graph.addEdge(v0, v3, 13);
+    graph.addEdge(v0, v4, 22);
+    graph.addEdge(v1, v2, 22);
+    graph.addEdge(v1, v3, 13);
+    graph.addEdge(v1, v4, 13);
+    graph.addEdge(v2, v3, 19);
+    graph.addEdge(v2, v4, 14);
+    graph.addEdge(v3, v4, 19);
+
+    return graph;
+  }
+  public static BaseGraph<? extends Edge> Rail() {
+    UndirectedGraph graph = new UndirectedGraph();
+    Vertex v0 = new Vertex(0);
+    Vertex v1 = new Vertex(1);
+    Vertex v2 = new Vertex(2);
+    Vertex v3 = new Vertex(3);
+    Vertex v4 = new Vertex(4);
+    Vertex v5 = new Vertex(5);
+    Vertex v6 = new Vertex(6);
+    Vertex v7 = new Vertex(7);
+    Vertex v8 = new Vertex(8);
+    Vertex v9 = new Vertex(9);
+
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+    graph.addVertex(v5);
+    graph.addVertex(v6);
+    graph.addVertex(v7);
+    graph.addVertex(v8);
+    graph.addVertex(v9);
+
+
+    graph.addEdge(v0, v1, 10);
+    graph.addEdge(v1, v2, 10);
+    graph.addEdge(v1, v6, 8);
+    graph.addEdge(v1, v7, 13);
+    graph.addEdge(v2, v3, 10);
+    graph.addEdge(v2, v7, 8);
+    graph.addEdge(v2, v8, 13);
+    graph.addEdge(v3, v4, 10);
+    graph.addEdge(v3, v8, 8);
+    graph.addEdge(v5, v6, 10);
+    graph.addEdge(v6, v7, 10);
+    graph.addEdge(v7, v8, 10);
+    graph.addEdge(v8, v9, 10);
+
+    return graph;
+  }
+  public static BaseGraph<? extends Edge> Tesselation() {
+    UndirectedGraph graph = new UndirectedGraph();
+    Vertex v0 = new Vertex(0);
+    Vertex v1 = new Vertex(1);
+    Vertex v2 = new Vertex(2);
+    Vertex v3 = new Vertex(3);
+    Vertex v4 = new Vertex(4);
+    Vertex v5 = new Vertex(5);
+    Vertex v6 = new Vertex(6);
+    Vertex v7 = new Vertex(7);
+    Vertex v8 = new Vertex(8);
+
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+    graph.addVertex(v5);
+    graph.addVertex(v6);
+    graph.addVertex(v7);
+    graph.addVertex(v8);
+
+
+    graph.addEdge(v0, v1, 8);
+    graph.addEdge(v0, v2, 12);
+    graph.addEdge(v1, v2, 13);
+    graph.addEdge(v1, v3, 25);
+    graph.addEdge(v1, v4, 9);
+    graph.addEdge(v2, v3, 14);
+    graph.addEdge(v2, v6, 21);
+    graph.addEdge(v3, v4, 20);
+    graph.addEdge(v3, v5, 8);
+    graph.addEdge(v3, v6, 12);
+    graph.addEdge(v3, v7, 12);
+    graph.addEdge(v3, v8, 16);
+    graph.addEdge(v4, v5, 19);
+    graph.addEdge(v5, v7, 11);
+    graph.addEdge(v6, v8, 11);
+    graph.addEdge(v7, v8, 9);
+
+    return graph;
+  }
+  public static BaseGraph<? extends Edge> CP4414() {
+    DirectedGraph graph = new DirectedGraph();
+    Vertex v0 = new Vertex(0);
+    Vertex v1 = new Vertex(1);
+    Vertex v2 = new Vertex(2);
+    Vertex v3 = new Vertex(3);
+    Vertex v4 = new Vertex(4);
+
+    graph.addVertex(v0);
+    graph.addVertex(v1);
+    graph.addVertex(v2);
+    graph.addVertex(v3);
+    graph.addVertex(v4);
+
+    graph.addEdge(v0, v1, 9);
+    graph.addEdge(v0, v2, 75);
+    graph.addEdge(v1, v2, 95);
+    graph.addEdge(v1, v4, 42);
+    graph.addEdge(v2, v3, 51);
+    graph.addEdge(v3, v1, 19);
+    graph.addEdge(v4, v3, 31);
 
     return graph;
   }
